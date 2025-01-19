ffa36a77e22156bb17e2621a8d562433de3560ae
herdieboy <thomasherd@me.com>
2025-01-19 23:52:12 +0100

    Add artwork search functionality and update layout styles

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'iip-thumb.smk.dk',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.smk.dk',
+      }
+    ],
+},
 };
 
 export default nextConfig;
