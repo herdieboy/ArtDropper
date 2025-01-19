9da17487cc0fad2fec09263770e0f9c969b485d0
herdieboy <thomasherd@me.com>
2025-01-20 00:27:34 +0100

    Refactor Work component to use getServerSideProps for data fetching and improve type definitions

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,32 +1,69 @@
 import Image from 'next/image'
+import { GetServerSideProps } from 'next'
 
-export default async function Work({ params } : { params: { id: string } }) {
-  const { id } = params
-  const data = await fetch(`https://api.smk.dk/api/v1/art/search/?keys=${id}&fields=object_number,titles,colors,image_thumbnail,production,production_date&filters=[has_image:true],[object_names:maleri],[public_domain:true]&offset=0&rows=1`)
-  const object = await data.json()
-  const items = object.items[0]
-  console.log(object)
+interface Params {
+  id: string;
+}
 
+interface WorkProps {
+  item: {
+    titles: { title: string }[];
+    colors: string[];
+    image_thumbnail: string;
+    production: { creator: string }[];
+    production_date: { period: string }[];
+  };
+}
+
+const Work = ({ item }: WorkProps) => {
   return (
     <div className="h-dvh flex flex-col justify-center items-center gap-[1rem]">
       <div className="relative w-[30rem] max-w-[90vw] aspect-square">
-        <Image src={object.items[0].image_thumbnail} fill className="object-contain" sizes="50vw" alt="" />
+        <Image
+          src={item.image_thumbnail}
+          fill
+          className="object-contain"
+          sizes="50vw"
+          alt=""
+        />
       </div>
 
       <div className="text-center">
-        <h1 className="text-white text-[2rem] ">{items.titles[0].title}</h1>
-        <h1 className="text-white">{items.production[0].creator}</h1>
-        <h1 className="text-white">{items.production_date[0].period}</h1>
+        <h1 className="text-white text-[2rem]">{item.titles[0].title}</h1>
+        <h1 className="text-white">{item.production[0].creator}</h1>
+        <h1 className="text-white">{item.production_date[0].period}</h1>
       </div>
 
       <div className="flex justify-center gap-[1rem] flex-wrap">
-        {items.colors.map((color: string) => (
-          <div key={color} style={{backgroundColor: color}} className="flex justify-center items-center w-[6rem] h-[6rem] rounded-[1rem] transition-all text-transparent hover:text-white">
-            <div className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-bold">{color}</div>
+        {item.colors.map((color: string) => (
+          <div
+            key={color}
+            style={{ backgroundColor: color }}
+            className="flex justify-center items-center w-[6rem] h-[6rem] rounded-[1rem] transition-all text-transparent hover:text-white"
+          >
+            <div className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-bold">
+              {color}
+            </div>
           </div>
         ))}
       </div>
-
     </div>
-  )
-}
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.params as Params; // Accessing params directly
+  const response = await fetch(
+    `https://api.smk.dk/api/v1/art/search/?keys=${id}&fields=object_number,titles,colors,image_thumbnail,production,production_date&filters=[has_image:true],[object_names:maleri],[public_domain:true]&offset=0&rows=1`
+  );
+  const object = await response.json();
+  const item = object.items[0];
+
+  return {
+    props: {
+      item,
+    },
+  };
+};
+
+export default Work;
